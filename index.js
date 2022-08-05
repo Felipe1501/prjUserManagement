45bb2be19d0b66e94327ebe45cdff1535c4bd480
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-05 11:40:41 -0300

    aula 41 add: if and else

--- index.js
+++ index.js
@@ -11,5 +11,25 @@ var admin = document.querySelector("#exampleAdmin");
 
 //a culpa é sempre sua sempre em questão de erro!!!
 
+var fields =  document.querySelectorAll("#form-user-create [name]");
 
+fields.forEach(function(field, index){
 
+    if(field.name == "gender"){
+        if(field.checked){
+            console.log("SIM", field);
+        }
+        
+    }else{
+        console.log("NÃO");
+    }
+
+});
+
+//console.log(field.id, field.name, field.value, field.checked, index);
+
+// = atribuição de valor
+
+//== comparar valores
+
+// === compara valor e o tipo de dado
