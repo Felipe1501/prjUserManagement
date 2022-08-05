0a7b1a8cf7ce0aa3079809f09b37c6bb67a3436b
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-05 11:22:05 -0300

    aula 38 add: variables and selectors

---
+++ index.js
@@ -0,0 +1,15 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassaword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleAdmin");
+
+
+
+//a culpa é sempre sua sempre em questão de erro!!!
+
+
+
