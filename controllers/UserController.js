b79b729b467f19e30d80d5528fb10db8d65e6079
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-08 10:54:34 -0300

    aula 64 fix: Working with Editing Data - programming the Event in the Edit Button.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,16 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEditCancel();
+    }
+
+    onEditCancel(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            //sempre que um trecho de código se repetir, Crie um Método dados variáveis serão parâmetros
+
+            this.showPanelCreate();
+
+        });
     }
 
     onSubmit(){
@@ -150,16 +160,33 @@ class UserController {
             <td>${(dataUser.admin) ? 'SIM' : 'NÃO'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
               <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
      
     `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+
+        });
         
         this.tableEl.appendChild(tr);
     
         this.updateCount();
     
+    }
+
+    showPanelCreate(){
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+    
+    showPanelUpdate(){
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
     updateCount(){
