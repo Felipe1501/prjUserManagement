3f039de8553d998dd44c48f6d6b6374b67fa2316
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-08 12:19:20 -0300

    aula 69 fix: deleting object data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -232,7 +232,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
               <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+              <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
      
     `;
@@ -246,6 +246,21 @@ class UserController {
     }
 
     addEventsTr(tr){
+
+        tr.querySelector(" .btn-delete").addEventListener("click", e => {
+            //confirm = abre uma janela de confirmação com ok e cancelar
+            if (confirm("Deseja realmente excluir?")){
+
+                tr.remove();
+
+                this.updateCount();
+
+            }
+
+
+        });
+
+
         tr.querySelector(".btn-edit").addEventListener("click", e=>{
 
             let json = JSON.parse(tr.dataset.user);
@@ -295,7 +310,7 @@ class UserController {
          });
          
 
-    }
+        }
 
     showPanelCreate(){
         document.querySelector("#box-user-create").style.display = "block";
