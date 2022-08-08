a6e855bfc49764cacf39dadd5f2f1c010a052858
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-08 10:35:29 -0300

    aula 62 fix: updating statistics - using dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -22,6 +22,8 @@ class UserController {
 
         let values = this.getValues();
 
+        if(!values) return false;
+
         this.getPhoto().then((content)=>{
             values.photo = content;
 
@@ -136,6 +138,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
 
             <td>
@@ -154,10 +158,30 @@ class UserController {
         
         this.tableEl.appendChild(tr);
     
-    
+        this.updateCount();
     
     }
 
+    updateCount(){
+        let numberUsers = 0;
+        let numberAdmin = 0;
 
+        [...this.tableEl.children].forEach(tr =>{
+
+            //operador++ = muito comum em laços de repetição(operador incremental)
+            numberUsers++;
+            //dataset = uma Api web que permite leitura e escrita em elementos co data-*
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+            //serialização = transforma um obj em texto
+
+        });
+
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+    }
 //método estático = pode ser chamada diretamente
 }
