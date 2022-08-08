51c095cf8de3c6403d6a81b00444a17cc62b8a71
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-08 09:21:50 -0300

    aula 58 fix: adjusting the project view - ternary operator

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -59,11 +59,17 @@ class UserController {
      resolve(fileReader.result);
      };
      
-     file.onerror = (e)=>{
+     fileReader.onerror = (e)=>{
         reject(e);
-     }
-     //callback = após a execução de uma rotina
+     };
+
+     if(file){
+        //callback = após a execução de uma rotina
      fileReader.readAsDataURL(file);
+     }else{
+        resolve('dist/img/boxed-bg.jpg');
+     }
+     
  
  
         });
@@ -81,6 +87,10 @@ class UserController {
                         user[field.name] = field.value;
                     }
                     
+                }else if(field.name == "admin"){
+
+                    user[field.name] = field.checked;
+
                 }else{
                     user[field.name] = field.value;
                 }
@@ -103,22 +113,25 @@ class UserController {
 
     addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+
             <td>
               <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
             </td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin) ? 'SIM' : 'NÃO'}</td>
             <td>${dataUser.data}</td>
             <td>
               <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
               <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
-    
+     
     `;
+        
+        this.tableEl.appendChild(tr);
     
     
     
